493d49ca1294da6978937970eb6d4d66e2d6a964
Yukki16 <89789213+Yukki16@users.noreply.github.com>
2022-05-10 12:43:31 +0200

    Done mostly everything

---
+++ GXPEngine/Laser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+
+namespace GXPEngine
+{
+    public class Laser : AnimationSprite
+    {
+        float timer = 0f;
+        public bool invisible = false;
+        public Laser(String name, int rows, int cols, TiledObject obj) : base(name, rows, cols, -1, true)
+        {
+            this.collider.isTrigger = true;
+        }
+
+        void Update()
+        {
+            if (Time.time - timer > 5000)
+            {
+                invisible = !invisible;
+                timer = Time.time;
+                if (invisible)
+                {
+                    this.visible = false;
+                    this.collider.isTrigger = false;
+                }
+                else
+                {
+                    this.visible = true;
+                    this.collider.isTrigger = true;
+                }
+            }
+
+            
+
+        }
+    }
+}
