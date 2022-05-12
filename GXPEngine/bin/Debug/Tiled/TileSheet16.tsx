da9f9b1a59bfcd165e1ae55ec5ca0c115920b005
Fernando Gramage <fergr30@gmail.com>
2022-05-12 12:12:13 +0200

    Updated Level 2

---
+++ GXPEngine/bin/Debug/Tiled/TileSheet16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="TileSheet16" tilewidth="16" tileheight="16" tilecount="299" columns="13">
+ <image source="TileSets/TileSheet16.png" width="208" height="368"/>
+</tileset>
